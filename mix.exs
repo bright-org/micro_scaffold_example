9e225d2b61ae0891862c83534afe47277a3f038a
BlackTanuTanuNinjas <challenge8tanu@gmail.com>
2026-04-01 11:48:14 +0900

    chore: add deps

--- mix.exs
+++ mix.exs
@@ -21,8 +21,8 @@ defmodule MicroScaffoldExample.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:atomvm_http_server,
+       git: "https://github.com/bright-org/micro_phoenix.git", branch: "develop"},
       {:ecto_sqlite3, "~> 0.21"}
     ]
   end
